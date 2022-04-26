0705a5b98fa9d514986f81d314bcd7a093250630
louisboilard <louis.boilard@hotmail.com>
2022-04-26 09:43:14 -0400

    infere lualine colorscheme based on current scheme

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -1,27 +1,11 @@
 -- tango is my custom theme in .local/share/nvim/plugged/lualine/lua/themes
 -- local custom_theme = require'lualine.themes.tango'
-local pyramid = require'lualine.themes.pyramid'
-
--- Change the background of lualine_c section to transparant for all modes
--- Most themes only have normal.c and inactive.c!
--- custom_theme.normal.c.bg = 'NONE'
--- custom_theme.insert.c.bg = 'NONE'
--- custom_theme.command.c.bg = 'NONE'
--- custom_theme.visual.c.bg = 'NONE'
--- custom_theme.inactive.c.bg = 'NONE'
-
--- i wanna make my own config. with the b section being non invisible but with some cool colors
--- custom_theme.normal.b.bg = 'NONE'
--- custom_theme.insert.b.bg = 'NONE'
--- custom_theme.command.b.bg = 'NONE'
--- custom_theme.visual.b.bg = 'NONE'
--- custom_theme.inactive.b.bg = 'NONE'
+-- local pyramid = require'lualine.themes.pyramid'
 
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    -- theme = 'auto', -- auto matches the nvim theme (neodark, onenord..)
-    theme = pyramid,
+    theme = 'auto', -- auto matches the nvim theme (neodark, onenord..)
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {},
