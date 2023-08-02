949db4444050505995b8cd23f14595b3f8bf093b
louisboilard <louis@gravitysketch.com>
2023-08-02 11:52:46 -0400

    Sketchybar conf

---
+++ sketchybar/plugins/zen.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+zen_on() {
+  sketchybar --set github.bell drawing=off \
+             --set apple.logo drawing=off \
+             --set calendar icon.drawing=off \
+             --set separator drawing=off \
+             --set front_app drawing=off \
+             --set volume_icon drawing=off \
+             --set spotify.anchor drawing=off \
+             --set spotify.play updates=off
+}
+
+zen_off() {
+  sketchybar --set github.bell drawing=on \
+             --set apple.logo drawing=on \
+             --set calendar icon.drawing=on \
+             --set separator drawing=on \
+             --set front_app drawing=on \
+             --set volume_icon drawing=on \
+             --set spotify.play updates=on
+}
+
+if [ "$1" = "on" ]; then
+  zen_on
+elif [ "$1" = "off" ]; then
+  zen_off
+else
+  if [ "$(sketchybar --query apple.logo | jq -r ".geometry.drawing")" = "on" ]; then
+    zen_on
+  else
+    zen_off
+  fi
+fi
